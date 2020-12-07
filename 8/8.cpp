0cebc3c71bf77930cedbf41068dcfa7b89505e2b
Михаил Кораблев <cstud110917@vyatsu.ru>
2020-12-07 16:49:34 +0300

    new

---
+++ 8/8.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    ifstream in("f.txt"); 
+    ofstream out("g.txt");
+    string sym; 
+    while (in.peek() != EOF) 
+    {
+        getline(in, sym); 
+        out << endl << sym << endl << "Количество символов в данной строке: " << sym.size() << endl;
+    }
+    in.close();
+    out.close();
+    system("pause");
+}
